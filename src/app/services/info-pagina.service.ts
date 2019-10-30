3c31735ba4259bf86cf032da89f55f5d363a6c6e
Daniel Rivera <edaniel@javerianacali.edu.co>
2019-10-30 10:45:06 -0500

    Fin firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
     constructor( private http: HttpClient) {
-      // console.log('información de pagina ok');
-      // Leer Json
-      this.http.get('assets/data/data-pages.json')
-        .subscribe( (resp: InfoPagina) => {
-
-            this.cargada = true;
-            this.info = resp;
-            console.log(resp);
-        });
-    }
+      this.cargarInfo();
+      this.cargarEquipo();
   }
+
+  // tslint:disable-next-line: align
+  private cargarInfo() {
+    this.http.get('assets/data/data-pages.json')
+          .subscribe( (resp: InfoPagina) => {
+
+              this.cargada = true;
+              this.info = resp;
+          });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-pruebas-99ff2.firebaseio.com/equipo.json')
+    // tslint:disable-next-line: align
+    .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        console.log(resp);
+
+      });
+    }
+}
